af99ce789f5ea0f303c2325558b5cf81eebce7b2
SunilKumar <sunil.2701k@gmail.com>
2025-09-28 11:23:11 +0530

    adding string program

--- src/rohansir/string/MainStr.java
+++ src/rohansir/string/MainStr.java
@@ -2,9 +2,33 @@ package rohansir.string;
 
 public class MainStr {
     public static void main(String[] args) {
-        String st = "Hello";
-        char[] ch = st.toCharArray();
-        System.out.println(ch);
+        String st1 = new String("pen");
+        String st2 = "pen";
+        System.out.println(st1 == st2); // false
+        System.out.println(st1.equals(st2));//true
+
+        System.out.println("------------------");
+
+        String s1 = new String("car");
+        String s2 = new String("car");
+        System.out.println(s1 == s2);//false
+        System.out.println(s1.equals(s2));//true
+
+        System.out.println("---------------");
+
+        String a1 = new String("HP");
+        String b1 = a1;
+        String c1 = b1;
+        System.out.println(a1 == b1);//true
+        System.out.println(b1.equals(c1));//true
+
+        System.out.println("----------------------");
+
+        String a = "AB";
+        String b = "A";
+        String c = b + "B";
+        System.out.println(a == c);//false
+        System.out.println(a.equals(c));//false
     }
 
 }
